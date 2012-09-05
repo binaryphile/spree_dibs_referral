e7c3a86ad3d8e25e5f2813182acd79788e3cd8e5
Ted Lilley <tlilley@diditbetter.com>
2012-09-05 17:24:55 -0400

    Move view strings to config/en.yml.  Fixes #3

--- app/overrides/add_referral_column_to_orders_index.rb
+++ app/overrides/add_referral_column_to_orders_index.rb
@@ -7,5 +7,5 @@ Deface::Override.new(virtual_path: 'spree/admin/orders/index',
 Deface::Override.new(virtual_path: 'spree/admin/orders/index',
                      name: 'add_referral_value_to_orders_index',
                      insert_before: 'td:contains(".total")',
-                     text: '<td><%= order.dibs_referral.nil? ? "" : (link_to order.dibs_referral.email, admin_user_path(order.dibs_referral)) %></td>'
+                     text: '<td><%= order.dibs_referral.nil? ? t(:nothing) : (link_to order.dibs_referral.email, admin_user_path(order.dibs_referral)) %></td>'
                     )

580a4b3ba79cc919f2b02712647d630f7191e1c4
Ted Lilley <tlilley@diditbetter.com>
2012-09-06 12:26:08 -0400

    Convert deface overrides to use deface dsl. Fixes #1

--- app/overrides/add_referral_select_to_checkout_payment_step.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(virtual_path: 'spree/checkout/_payment',
-                     name: 'add_referral_select_to_checkout_payment_step',
-                     insert_before: 'fieldset#payment',
-                     text: '<fieldset id="billing"><legend><%= t(:referral) %></legend><div><p><%= t(:referral_checkout_message) %></p><%= form.collection_select :dibs_referral_id, Spree::User.dibs_referrers, :id, :email, { include_blank: true } %></div></fieldset>'
-                    )
--- app/overrides/add_referral_to_admin_order_tab_summary.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(virtual_path: 'spree/admin/shared/_order_tabs',
-                     name: 'add_referral_to_admin_order_tab_summary',
-                     insert_after: '#payment_status',
-                     text: '<h5 id="dibs_referral" style="width: 100%;"><%= t(:referral) %>: <%= @order.dibs_referral.nil? ? t(:nothing) : @order.dibs_referral.email %></h5>'
-                    )

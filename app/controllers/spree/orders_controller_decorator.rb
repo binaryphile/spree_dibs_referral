c6c872f5bacb246a47c0a513c3b0a7239561f20e
Ted Lilley <tlilley@diditbetter.com>
2012-10-12 19:28:39 -0400

    Add working referral button for carts, change product referral link to button

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -1,43 +1,28 @@
 Spree::OrdersController.class_eval do
-  after_filter :dibs_capture_referral, only: :edit
+  before_filter :dibs_edit, only: :edit
 
-  private
-    def dibs_capture_referral
-      if session[:dibs_referral] and (!@current_order.dibs_referral or session[:dibs_referral] != @current_order.dibs_referral.email)
-        @current_order.dibs_referral = Spree::User.find_by_email(session[:dibs_referral])
-        @current_order.save
-      end
-    end
-
-    def check_authorization
-      debugger
-      session[:access_token] ||= params[:token]
-      request_order =  Spree::Order.find_by_number(params[:id])
-      order = current_order
-      if request_order.nil? and order.nil?
-        authorize! :create, Spree::Order.new
-      else
-        if request_order.nil?
-          authorize! :edit, order, session[:access_token]
+  def dibs_edit
+    if params[:id] and params[:token]
+      request_order = Spree::Order.find_by_number(params[:id])
+      if request_order and authorize!(:edit, request_order, params[:token])
+        if request_order.user == current_user
+          @current_order = request_order
         else
-          if request_order.user == current_user
-            authorize! :edit, request_order, session[:access_token]
-          else
-            authorize! :create, Spree::Order.new
-            dibs_clone request_order
-          end
+          @current_order = dibs_clone request_order
         end
       end
     end
+  end
 
+  private
     def dibs_clone(order)
-      debugger
-      my_order = current_order(true)
+      my_order = new_order
       my_order.line_items.destroy_all
       order.line_items.each do |line_item|
-        new_order.add_variant(line_item.variant, line_item.quantity)
+        my_order.add_variant(line_item.variant, line_item.quantity)
       end
       my_order.dibs_referral = order.user
+      my_order.save
       session[:order_id] = my_order.id
       my_order
     end

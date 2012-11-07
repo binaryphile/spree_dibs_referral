dc261b7f4b271d01aee751f5d28f32f87ecf9e8f
Ted Lilley <tlilley@diditbetter.com>
2012-11-07 17:02:03 -0500

    Add spree_flexi_variants support (optional)

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -3,6 +3,15 @@ Spree::Order.class_eval do
   attr_accessible :dibs_referral_id
 
   def dibs_referral_line_items
-    line_items.inject({}) { |result, item| result.update("variants[#{item.variant.id}]" => item.quantity) }
+    result = {}
+    line_items.each do | item |
+      result.update("variants[#{item.variant.id}]" => item.quantity)
+      if item.respond_to?(:ad_hoc_option_values) and item.ad_hoc_option_values and !item.ad_hoc_option_values.empty?
+        item.ad_hoc_option_values.each do | option |
+          result.update("ad_hoc_option_values[#{option.ad_hoc_option_type.id}]" => option.id)
+        end
+      end
+    end
+    result
   end
 end

3254484e4645a0591f040768fa20450e0a10547d
Ted Lilley <tlilley@diditbetter.com>
2013-05-23 17:31:58 +0000

    Refactor referral profile into host application

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Spree::Core::Engine.routes.draw do
   # Add your extension routes here
   match '/dibs-referral', to: 'orders#populate'
-  match '/dibs-referral-request', to: 'users#dibs_referral_request'
-  match '/dibs-referral-profile', to: 'users#dibs_referral_profile'
 end
 

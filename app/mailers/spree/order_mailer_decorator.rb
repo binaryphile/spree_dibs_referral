c4c0b3fb0eeaa58bd95f90fc8ecec1657a865d44
Ted Lilley <tlilley@diditbetter.com>
2013-08-15 21:21:18 +0000

    Add from address to emails for Spree update

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -1,23 +1,28 @@
 Spree::OrderMailer.class_eval do
   def confirm_email(order, resend = false)
-    @order = order
+    find_order(order)
     subject = (resend ? "[#{t(:resend).upcase}] " : '')
     subject += "#{Spree::Config[:site_name]} #{t('order_mailer.confirm_email.subject')} ##{order.number}"
     if order.dibs_referral
-      mail(to: order.email, subject: subject, cc: order.dibs_referral.email)
+      mail(to: order.email, from: from_address, subject: subject, cc: order.dibs_referral.email)
     else
-      mail(to: order.email, subject: subject)
+      mail(to: order.email, from: from_address, subject: subject)
     end
   end
 
   def cancel_email(order, resend = false)
-    @order = order
+    find_order(order)
     subject = (resend ? "[#{t(:resend).upcase}] " : '')
     subject += "#{Spree::Config[:site_name]} #{t('order_mailer.cancel_email.subject')} ##{order.number}"
     if order.dibs_referral
-      mail(to: order.email, subject: subject, cc: order.dibs_referral.email)
+      mail(to: order.email, from: from_address, subject: subject, cc: order.dibs_referral.email)
     else
-      mail(to: order.email, subject: subject)
+      mail(to: order.email, from: from_address, subject: subject)
     end
+
+    private
+      def find_order(order)
+        @order = order.is_a?(Spree::Order) ? order : Spree::Order.find(order)
+      end
   end
 end
